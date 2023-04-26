80a1bb2028c3ab1d8b3ad163b2c4872c1a03b69e
Camilo Molano <camilo@algorand.foundation>
2023-04-26 09:27:58 -0500

    If and cond

--- app.py
+++ app.py
@@ -1,15 +1,80 @@
 from beaker import *
 from pyteal import *
 
-app = Application("HelloWorld")
+
+class MiEstado:
+    total = GlobalStateValue(TealType.uint64)
+    edad = GlobalStateValue(TealType.uint64)
+        
+app = (
+    Application("Calculator", state=MiEstado())
+    .apply(unconditional_create_approval, initialize_global_state=True)
+    #.apply(unconditional_opt_in_approval, initialize_local_state=True)
+)
 
 @app.external
 def hello(name: abi.String, *, output: abi.String) -> Expr:
     return output.set(Concat(Bytes("Hello, "), name.get()))
 
-@app.delete(bare=True, authorize=Authorize.only(Global.creator_address()))
-def delete() -> Expr:
-    return Approve()
+@app.external
+def suma (numero1: abi.Uint64, numero2: abi.Uint64, *, output: abi.Uint64) -> Expr:
+    return Seq(
+        output.set(numero1.get()+ numero2.get()),
+        app.state.total.set(numero1.get()+ numero2.get())
+    )
+
+@app.external
+def division (numero1: abi.Uint64, numero2: abi.Uint64, *, output: abi.Uint64) -> Expr:
+     return Seq(
+        Assert(
+            numero2.get() >= Int(1),
+        ),
+        output.set(numero1.get()/ numero2.get()),
+        app.state.total.set(numero1.get() / numero2.get())
+    )
+
+@app.external
+def validar_Edad(edad: abi.Uint64, *, output: abi.String) -> Expr:
+    return (
+        Cond(
+        [ edad.get()>=Int(18),
+            Seq(
+                app.state.edad.set(edad.get()),
+                output.set(Bytes("Ingreso permitido"))
+            )
+        ],
+        [ edad.get()<Int(18),
+            Seq(
+                app.state.edad.set(edad.get()),
+                output.set(Bytes("Ingreso no permitido"))
+            )
+        ]
+        )
+    )
+
+@app.external
+def validar_Edad_If(edad: abi.Uint64, *, output: abi.String) -> Expr:
+    return (
+        If(
+            edad.get()>=Int(18),
+            Seq(
+                app.state.edad.set(edad.get()),
+                output.set(Bytes("Ingreso permitido"))
+            ), 
+            Seq(
+                app.state.edad.set(edad.get()),
+                output.set(Bytes("Ingreso no permitido"))
+            )
+        )
+    )
+
+@app.external
+def leer_Edad(*, output: abi.Uint64) -> Expr:
+    return output.set(app.state.edad.get())
+
+@app.external
+def leer_resultado(*,output:abi.Uint64) -> Expr:
+    return  output.set(app.state.total.get())
 
 if __name__ == "__main__":
     app.build().export("./artifacts")
